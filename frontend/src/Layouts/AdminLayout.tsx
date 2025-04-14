50f6148b8e31f94a0935ecc0b3df87a0d3d57c6f
ChaudaryAbdullah <abdullahshakir226@gmail.com>
2025-04-14 20:04:49 +0500

    adding more user components

--- frontend/src/Layouts/AdminLayout.tsx
+++ frontend/src/Layouts/AdminLayout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "../Components/Admin/AdminDashboard";
 import Courses from "../Components/Admin/Courses";
+// import UserManagement from "../Components/Admin/UserManagement";
+// import Requests from "../Components/Admin/Requests";
 
 const AdminLayout: React.FC = () => {
   return (
@@ -10,6 +12,8 @@ const AdminLayout: React.FC = () => {
         <Routes>
           <Route path="/" element={<AdminDashboard />} />
           <Route path="/courses" element={<Courses />} />
+          {/* <Route path="/requests" element={<Requests />} />
+          <Route path="/user-management" element={<UserManagement />} /> */}
         </Routes>
       </div>
     </div>
--- frontend/src/Layouts/StudentLayout.tsx
+++ frontend/src/Layouts/StudentLayout.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../Components/User/Dashboard";
 import Account from "../Components/User/Account";
+import Courses from "../Components/User/Courses";
+// import Career from "../Components/User/Career";
+// import StudyMaterial from "../Components/User/StudyMaterial";
+// import JobInternship from "../Components/User/JobInternship";
+// import Feedback from "../Components/User/Feedback";
 
 const StudentLayout: React.FC = () => {
   return (
@@ -9,8 +14,12 @@ const StudentLayout: React.FC = () => {
       <div className="flex-grow bg-gray-100">
         <Routes>
           <Route path="/" element={<Dashboard />} />
-
+          <Route path="/courses" element={<Courses />} />
           <Route path="/account" element={<Account />} />
+          {/* <Route path='/career' element={<Career />} />    
+          <Route path='/study-material' element={<StudyMaterial />} />    
+          <Route path='/job-internship' element={<JobInternship />} />    
+          <Route path='/feedback' element={<Feedback />} />     */}
         </Routes>
       </div>
     </div>
