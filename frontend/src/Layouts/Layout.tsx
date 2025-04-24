8b78d7cb84427b24cfcfd071aa7a1cb1d87c8547
ChaudaryAbdullah <abdullahshakir226@gmail.com>
2025-04-24 21:26:23 +0500

    Employee management added

--- frontend/src/Layouts/Layout.tsx
+++ frontend/src/Layouts/Layout.tsx
@@ -1,46 +1,27 @@
-import React from "react";
-import Sidebar from "../Components/User/Sidebar";
+import React from "react"
+import Sidebar from "../Components/User/Sidebar"
 import { useLocation } from "react-router-dom";
 import { Paths } from "../Utils/types";
 import authService from "../Components/Auth/authService";
 import AdminSidebar from "../Components/Admin/SideBar";
 import IndustrySidebar from "../Components/Industry/Sidebar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout ({ children }: { children: React.ReactNode }) {
   const location = useLocation();
   const role = authService.getRole();
+ 
+    return (
+      <div className='flex w-full'>
+        {
+          (location.pathname!=="/login" && location.pathname!=="/signup")&&
+          (role ==="Student"  ? <Sidebar route={(Paths[location.pathname as keyof typeof Paths] ? location.pathname : "/") as keyof typeof Paths} />: 
+          role === "Admin" ? <AdminSidebar route={(Paths[location.pathname as keyof typeof Paths] ? location.pathname : "/") as keyof typeof Paths} />: 
+          <IndustrySidebar route={(Paths[location.pathname as keyof typeof Paths] ? location.pathname : "/") as keyof typeof Paths} />)
+        }
 
-  return (
-    <div className="flex w-full">
-      {location.pathname !== "/login" &&
-        location.pathname !== "/signup" &&
-        (role === "Student" ? (
-          <Sidebar
-            route={
-              (Paths[location.pathname as keyof typeof Paths]
-                ? location.pathname
-                : "/") as keyof typeof Paths
-            }
-          />
-        ) : role === "Admin" ? (
-          <AdminSidebar
-            route={
-              (Paths[location.pathname as keyof typeof Paths]
-                ? location.pathname
-                : "/") as keyof typeof Paths
-            }
-          />
-        ) : (
-          <IndustrySidebar
-            route={
-              (Paths[location.pathname as keyof typeof Paths]
-                ? location.pathname
-                : "/") as keyof typeof Paths
-            }
-          />
-        ))}
-
-      <div className="w-full">{children}</div>
-    </div>
-  );
-}
+        <div className='w-full'>
+          {children}
+        </div>
+      </div>
+    )
+  }
